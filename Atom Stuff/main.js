178d5b3f9e0ee8a6054089c151c0d2bdb3f63b3f
Gio Jacobsen <superpieman157@gmail.com>
2019-06-02 13:32:13 -0500

    Start

---
+++ Atom Stuff/main.js
@@ -0,0 +1,33 @@
+var gameData = {
+  Money: 0,
+  MoneyPerClick: 1,
+  moneyPerClickCost: 10
+}
+
+function makeMoney() {
+  gameData.Money += gameData.MoneyPerClick
+  document.getElementById("MoneyMade").innerHTML = gameData.Money + " Money Made"
+}
+
+function buyMoneyPerClick() {
+  if (gameData.Money >= gameData.moneyPerClickCost){
+    gameData.Money -= gameData.moneyPerClickCost
+    gameData.MoneyPerClick += 1
+    gameData.moneyPerClickCost *= 2
+    document.getElementById("MoneyMade").innerHTML = gameData.Money + " Money Made"
+    document.getElementById("perClickUpgrade").innerHTML = "Upgrade Skill (Now Level" + gameData.MoneyPerClick + ") Cost: " + gameData.moneyPerClickCost + " Money"
+  }
+}
+
+var mainGameLoop = window.setInterval(function() {
+  makeMoney()
+}, 1000)
+
+var saveGameAuto = window.setInterval(function() {
+  localStorage.setItem('MoneyMakerSave', JSON.stringify(gameData))
+}, 15000)
+
+var saveGame = JSON.parse(localStorage.getItem("MoneyMakerSave"))
+if (saveGame !== null){
+  gameData = saveGame
+}
